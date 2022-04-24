d4b44e558ec3511bff803d897097be46f433f842
dpurser-dev <david.purser@kellogg.northwestern.edu>
2022-04-24 22:37:53 +0000

    Completed

--- practice.rb
+++ practice.rb
@@ -49,10 +49,32 @@ end
 # CHALLENGE
 # Can you display the weather forecast summary for a user-entered city?
 # Use the following code at the very top of the file and then replace "chicago" in the api url with the user-entered city:
-#puts "What city are you in?"
-#city = gets.chomp
-#puts city
-city = "Chicago"
 
-#puts weather_data["currentConditions"]["temp"]["c"]
-# Note: what happens if the user-entered value is not a known city? You'll want to do some error handling.
+# Ask the user what city they are in
+puts "What city are you in?"
+# Store the input and remove whitespace to make compatible with browser
+city = gets.chomp.gsub(/\s+/, "")
+
+# Construct a URL and access the API
+url = "https://weatherdbi.herokuapp.com/data/weather/#{city.downcase}"
+uri = URI(url)
+response = Net::HTTP.get(uri)
+weather_data_user = JSON.parse(response)
+
+# Print the result with error handling
+if weather_data_user["status"] == "fail" then
+    puts "Sorry, that city is not recognized. Please try again"
+else
+    puts "In #{weather_data_user["region"]} it is currently #{weather_data_user["currentConditions"]["temp"]["f"]} degrees and #{weather_data_user["currentConditions"]["comment"].downcase}."
+    puts "The rest of today will be a high of #{weather_data_user["next_days"][0]["max_temp"]["f"]} and #{weather_data_user["next_days"][0]["comment"].downcase}."
+    puts "The upcoming weather forecast is:"
+    index = 1
+    loop do
+        if index == weather_data_user["next_days"].size
+            break
+        else     
+            puts "#{weather_data_user["next_days"][index]["day"]}: a high of #{weather_data_user["next_days"][index]["max_temp"]["f"]} and #{weather_data_user["next_days"][index]["comment"].downcase}."
+        index = index + 1
+        end
+    end
+end
